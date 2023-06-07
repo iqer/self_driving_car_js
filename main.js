2a5a429a808d04da196eaff7eb806b098bba82ad
hanno <cqh0718@gmail.com>
2023-06-07 15:03:32 +0800

    feat: impl collision detection

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ canvas.width = 200
 const ctx = canvas.getContext('2d')
 const road = new Road(canvas.width / 2, canvas.width * 0.9)
 const car = new Car(road.getLaneCenter(1), 100, 30, 50) // x, y, width, height
-// car.draw(ctx)
 animate()
 
 function animate () {
